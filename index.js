d0f4932eb0ca1f83b6dd99b43d03e86737adcc32
Manuel Escobedo Lopez <manuelfx117@gmail.com>
2022-01-31 10:14:14 -0600

    awesome feature

--- index.js
+++ index.js
@@ -2,7 +2,11 @@ const express = require("express");
 const app = express();
 
 app.get("/", (_, res) => {
-  res.json({ ok: true, message: "Hello World" });
+  res.json({
+    ok: true,
+    message: "Hello World",
+    feature: "this is an important feature, enjoy it!",
+  });
 });
 
 const port = process.env.PORT || 5000;
